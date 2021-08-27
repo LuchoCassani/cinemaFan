dedb711abe35939536455e87f68993bac14070b5
Lucho Cassani <lucho_92@hotmail.com>
2021-08-26 22:31:38 -0300

    actualizacion

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+/*
 function datos (){
     let nombre = prompt("ingresa tu nombre");
     let apellido = prompt ("ingresa tu apellido");
@@ -45,4 +46,81 @@ datos()
 
 recomendacion()
 
+*/
+
+// OBEJTOS
+
+const pelicula1 = {
+    nombre: "Black Widow",
+    genero: "accion",
+    duracion: "2h 14min"
+};
+const pelicula2 = {
+    nombre: "Blackpink",
+    genero: "documental",
+    duracion: "1h 41min"
+};
+const pelicula3 = {
+    nombre: "Cruella",
+    genero: "fantasia",
+    duracion: "2h 14min"
+};
+const pelicula4 = {
+    nombre: "El Escuadron Suicida",
+    genero: "accion",
+    duracion: "2h 13min"
+};
+const pelicula5 = {
+    nombre: "El Padre",
+    genero: "drama",
+    duracion: "1h 37min"
+};
+const pelicula6 = {
+    nombre: "Free Guy",
+    genero: "ciencia ficcion",
+    duracion: "1h 55min"
+};
+const pelicula7 = {
+    nombre: "Rapidos y Furiosos 9",
+    genero: "accion",
+    duracion: "2h 25min"
+};
+const pelicula8 = {
+    nombre: "Jungle Cruise",
+    genero: "aventura",
+    duracion: "2h 42min"
+};
+const pelicula9 = {
+    nombre: "La Purga",
+    genero: "suspenso",
+    duracion: "1h 44min"
+};
+
+//class constructora
+
+class Peliculas {
+    constructor(nombre, genero, duracion, precio) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.duracion = duracion;
+        this.precio = parseFloat(precio);
+    }
+    datosPeliculas(){
+        console.log("La pelicula "+ this.nombre + "pertenece al genero " + this.genero + " y tiene una duracion de " + this.duracion + ". Su valor es de " + (this.precio= this.precio*1.21) + " pesos")
+    }
+    
+}
+const pelicula10 = new Peliculas ("Space Jam ",  "animacion/infantil", "2h", 350);
+const pelicula11 = new Peliculas ("Candyman ",  "terror",  "2h", 555);
+const pelicula12 = new Peliculas ("Cry Macho ",  "drama",  "2h 30min", 666);
+const pelicula13 = new Peliculas ("Viejos ",  "suspenso",  "1h 58min", 425);
+const pelicula14 = new Peliculas ("Reminiscencia ","ciencia ficcion",  "2h 28min", 777);
+const pelicula15 = new Peliculas ("Venom ",  "accion",  "2h 20min", 620);
+
+pelicula10.datosPeliculas();
+
+const arrayPelis = ["pelicula10", "pelicula11", "pelicula12", "pelicula13", "pelicula14", "pelicula15"];
+for (const pelis of arrayPelis){
+    console.log(pelis)
+}
 
